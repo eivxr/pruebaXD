ea0d17098e6f3d49f4cf0c2584bbb7fa6b129926
Eivar Ulises Cuevas <putyfive@gmail.com>
2023-06-12 21:49:26 -0600

    fix: img directories

--- src/components/Hangman.jsx
+++ src/components/Hangman.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
+import '../../src/img/0.png';
 import '../../src/img/1.png';
+import '../../src/img/2.png';
+import '../../src/img/3.png';
+import '../../src/img/4.png';
+import '../../src/img/5.png';
+import '../../src/img/6.png';
 import './../App.css'
 
 export const Hangman = ({guesses}) => {
